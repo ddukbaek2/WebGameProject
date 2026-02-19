3053a076a5043bfea03e51da4faca77bf99ce38e
ddukbaek2 <ddukbaek2@gmail.com>
2026-02-19 10:26:00 +0900

    작업.

---
+++ Assets/Scripts/RuntimeInitializer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+namespace WebGameProject
+{
+	/// <summary>
+	/// 런타임 초기화 처리기.
+	/// </summary>
+	public static class RuntimeInitializer
+	{
+		/// <summary>
+		/// 초기화됨.
+		/// </summary>
+		[RuntimeInitializeOnLoadMethod]
+		private static void Initialize()
+		{
+			
+		}
+	}
+}
